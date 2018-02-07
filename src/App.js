3ec62a4fbba6d300afc0bb4d14092bc5a9f9681c
justcode365 <justcode365@163.com>
2018-02-08 02:52:33 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import './App.css'
+import TreeNew from './generateTree'
 
-class App extends Component {
+const tree = new TreeNew()
+const time_start = new Date().getTime()
+
+const fetchData = async id => {
+  const res = await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+  )
+  const item = await res.json()
+  if (!item.kids) {
+    tree.add(item.id, item.parent)
+    console.warn(new Date().getTime() - time_start)
+
+    return
+  }
+  item.kids.map(id => {
+    fetchData(id)
+  })
+}
+
+tree.add(16323105)
+fetchData(16323105)
+
+const Tree = ({ node }) => {
+  if (node.children.length === 0) {
+    return node.data
+  }
+  return (
+    <div>
+      <div className="parent">{node.data}</div>
+      {node.children.map(n => (
+        <div key={n.data}>
+          <Tree node={n} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+class AsyncTree extends React.Component {
+  state = { item: {} }
+  async componentDidMount() {
+    const { id } = this.props
+
+    const res = await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+    )
+    const item = await res.json()
+    this.setState({ item })
+  }
+
   render() {
+    const { item } = this.state
+    if (Object.keys(item).length === 0) return ''
+    // console.warn(new Date().getTime() - this.props.time)
+    if (!item.kids) {
+      return item.id
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="parent">{item.id}</div>
+        {item.kids.map(id => (
+          <div key={id}>
+            <AsyncTree id={id} time={this.props.time} />
+          </div>
+        ))}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    const time = new Date().getTime()
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tree View</h1>
+        </header>
+        <main>{/* <Tree node={tree.root} /> */}</main>
+        <hr />
+        <main>{<AsyncTree id={16323105} time={time} />}</main>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/generateTree.js
@@ -0,0 +1,158 @@
+function Node(data) {
+  this.data = data
+  this.children = []
+}
+
+class Tree {
+  constructor() {
+    this.root = null
+  }
+
+  add(data, toNodeData) {
+    const node = new Node(data)
+    const parent = toNodeData ? this.findBFS(toNodeData) : null
+    if (parent) {
+      parent.children.push(node)
+    } else {
+      if (!this.root) {
+        this.root = node
+      } else {
+        return 'Root node is already assigned'
+      }
+    }
+  }
+
+  remove(data) {
+    if (this.root.data === data) {
+      this.root = null
+    }
+
+    const queue = [this.root]
+    while (queue.length) {
+      const node = queue.shift()
+      for (let i = 0; i < node.children.length; i++) {
+        if (node.children[i].data === data) {
+          node.children.splice(i, 1)
+        } else {
+          queue.push(node.children[i])
+        }
+      }
+    }
+  }
+
+  contains(data) {
+    return this.findBFS(data) ? true : false
+  }
+
+  findBFS(data) {
+    const queue = [this.root]
+    while (queue.length) {
+      const node = queue.shift()
+      if (node.data === data) {
+        return node
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i])
+      }
+    }
+    return null
+  }
+
+  _preOrder(node, fn) {
+    if (node) {
+      if (fn) {
+        fn(node)
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        this._preOrder(node.children[i], fn)
+      }
+    }
+  }
+
+  _postOrder(node, fn) {
+    if (node) {
+      for (let i = 0; i < node.children.length; i++) {
+        this._postOrder(node.children[i], fn)
+      }
+      if (fn) {
+        fn(node)
+      }
+    }
+  }
+
+  traverseDFS(fn, method) {
+    const current = this.root
+    if (method) {
+      this[`_${method}`](current, fn)
+    } else {
+      this._preOrder(current, fn)
+    }
+  }
+
+  traverseBFS(fn) {
+    const queue = [this.root]
+    while (queue.length) {
+      const node = queue.shift()
+      if (fn) {
+        fn(node)
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i])
+      }
+    }
+  }
+
+  print() {
+    if (!this.root) {
+      return console.log('No root node found')
+    }
+    const newline = new Node('|')
+    const queue = [this.root, newline]
+    let string = ''
+    while (queue.length) {
+      const node = queue.shift()
+      string += `${node.data.toString()} `
+      if (node === newline && queue.length) {
+        queue.push(newline)
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i])
+      }
+    }
+    console.log(string.slice(0, -2).trim())
+  }
+
+  printByLevel() {
+    if (!this.root) {
+      return console.log('No root node found')
+    }
+    const newline = new Node('\n')
+    const queue = [this.root, newline]
+    let string = ''
+    while (queue.length) {
+      const node = queue.shift()
+      string += node.data.toString() + (node.data !== '\n' ? ' ' : '')
+      if (node === newline && queue.length) {
+        queue.push(newline)
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i])
+      }
+    }
+    console.log(string.trim())
+  }
+}
+
+// const tree = new Tree()
+// tree.add('0')
+// tree.add('0-0', '0')
+// tree.add('0-0-0', '0-0')
+// tree.add('0-0-0-0', '0-0-0')
+// tree.add('0-0-1', '0-0')
+// tree.add('0-0-2', '0-0')
+// tree.add('0-1', '0')
+// tree.add('0-1-0', '0-1')
+// tree.add('0-1-1', '0-1')
+// tree.add('0-2', '0')
+
+export default Tree
